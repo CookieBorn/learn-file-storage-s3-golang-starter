03c61fd1d495199bdb8b678f8ab881b133b1b647
CookieBorn <robanikin@hotmail.com>
2025-04-19 07:40:42 +1000

    C: Video Handles

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,155 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const maxMemory = 1 << 30
+	var maxReader io.ReadCloser
+	maxReader = http.MaxBytesReader(w, maxReader, maxMemory)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get video data", err)
+		return
+	}
+
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unnorthurized", err)
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	defer file.Close()
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't upload video data", err)
+		return
+	}
+
+	filetype := header.Header.Get("Content-Type")
+
+	Type, _, err := mime.ParseMediaType(filetype)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Mime parse error", err)
+		return
+	}
+
+	if Type != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Inncorect file type. Expecting mp4", nil)
+		return
+	}
+
+	vid, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File create error", err)
+		return
+	}
+	defer os.Remove("tubely-upload.mp4")
+	defer vid.Close()
+
+	_, err = io.Copy(vid, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File copy error", err)
+		return
+	}
+
+	vidProccessed, err := processVideoForFastStart(vid.Name())
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File proccess error", err)
+		return
+	}
+	vidP, err := os.Open(vidProccessed)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File proccess open error", err)
+		return
+	}
+	defer os.Remove(vidProccessed)
+	defer vidP.Close()
+
+	_, err = vid.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File seek start error", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	url := base64.RawURLEncoding.EncodeToString(key)
+
+	aspect, err := getVideoAspectRatio(vid.Name())
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Aspect ratio error", err)
+		return
+	}
+
+	aspectString := "other"
+
+	if aspect == "16:9" {
+		aspectString = "landscape"
+	} else if aspect == "9:16" {
+		aspectString = "portrait"
+	}
+
+	fileKey := aspectString + "/" + url + ".mp4"
+
+	putParams := s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &fileKey,
+		Body:        vidP,
+		ContentType: &Type,
+	}
+
+	_, err = cfg.s3client.PutObject(r.Context(), &putParams)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File put error", err)
+		return
+	}
+
+	videoURL := "https://" + cfg.s3Bucket + ".s3." + cfg.s3Region + ".amazonaws.com/" + fileKey
+
+	updateVideoData := database.Video{
+		ID:                videoData.ID,
+		CreatedAt:         videoData.CreatedAt,
+		UpdatedAt:         time.Now(),
+		ThumbnailURL:      videoData.ThumbnailURL,
+		VideoURL:          &videoURL,
+		CreateVideoParams: videoData.CreateVideoParams,
+	}
+
+	err = cfg.db.UpdateVideo(updateVideoData)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Update video error", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"os/exec"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 
@@ -22,6 +30,7 @@ type apiConfig struct {
 	s3Region         string
 	s3CfDistribution string
 	port             string
+	s3client         *s3.Client
 }
 
 type thumbnail struct {
@@ -84,6 +93,13 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	awsCFG, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatalf("Couldn't load awsConfig: %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(awsCFG)
+
 	cfg := apiConfig{
 		db:               db,
 		jwtSecret:        jwtSecret,
@@ -94,6 +110,7 @@ func main() {
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
+		s3client:         s3Client,
 	}
 
 	err = cfg.ensureAssetsDir()
@@ -131,4 +148,126 @@ func main() {
 
 	log.Printf("Serving on: http://localhost:%s/app/\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+type VideoStruct struct {
+	Streams []struct {
+		Index              int    `json:"index"`
+		CodecName          string `json:"codec_name,omitempty"`
+		CodecLongName      string `json:"codec_long_name,omitempty"`
+		Profile            string `json:"profile,omitempty"`
+		CodecType          string `json:"codec_type"`
+		CodecTagString     string `json:"codec_tag_string"`
+		CodecTag           string `json:"codec_tag"`
+		Width              int    `json:"width,omitempty"`
+		Height             int    `json:"height,omitempty"`
+		CodedWidth         int    `json:"coded_width,omitempty"`
+		CodedHeight        int    `json:"coded_height,omitempty"`
+		ClosedCaptions     int    `json:"closed_captions,omitempty"`
+		FilmGrain          int    `json:"film_grain,omitempty"`
+		HasBFrames         int    `json:"has_b_frames,omitempty"`
+		SampleAspectRatio  string `json:"sample_aspect_ratio,omitempty"`
+		DisplayAspectRatio string `json:"display_aspect_ratio,omitempty"`
+		PixFmt             string `json:"pix_fmt,omitempty"`
+		Level              int    `json:"level,omitempty"`
+		ColorRange         string `json:"color_range,omitempty"`
+		ColorSpace         string `json:"color_space,omitempty"`
+		ColorTransfer      string `json:"color_transfer,omitempty"`
+		ColorPrimaries     string `json:"color_primaries,omitempty"`
+		ChromaLocation     string `json:"chroma_location,omitempty"`
+		FieldOrder         string `json:"field_order,omitempty"`
+		Refs               int    `json:"refs,omitempty"`
+		IsAvc              string `json:"is_avc,omitempty"`
+		NalLengthSize      string `json:"nal_length_size,omitempty"`
+		ID                 string `json:"id"`
+		RFrameRate         string `json:"r_frame_rate"`
+		AvgFrameRate       string `json:"avg_frame_rate"`
+		TimeBase           string `json:"time_base"`
+		StartPts           int    `json:"start_pts"`
+		StartTime          string `json:"start_time"`
+		DurationTs         int    `json:"duration_ts"`
+		Duration           string `json:"duration"`
+		BitRate            string `json:"bit_rate,omitempty"`
+		BitsPerRawSample   string `json:"bits_per_raw_sample,omitempty"`
+		NbFrames           string `json:"nb_frames"`
+		ExtradataSize      int    `json:"extradata_size"`
+		Disposition        struct {
+			Default         int `json:"default"`
+			Dub             int `json:"dub"`
+			Original        int `json:"original"`
+			Comment         int `json:"comment"`
+			Lyrics          int `json:"lyrics"`
+			Karaoke         int `json:"karaoke"`
+			Forced          int `json:"forced"`
+			HearingImpaired int `json:"hearing_impaired"`
+			VisualImpaired  int `json:"visual_impaired"`
+			CleanEffects    int `json:"clean_effects"`
+			AttachedPic     int `json:"attached_pic"`
+			TimedThumbnails int `json:"timed_thumbnails"`
+			NonDiegetic     int `json:"non_diegetic"`
+			Captions        int `json:"captions"`
+			Descriptions    int `json:"descriptions"`
+			Metadata        int `json:"metadata"`
+			Dependent       int `json:"dependent"`
+			StillImage      int `json:"still_image"`
+		} `json:"disposition"`
+		Tags struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			VendorID    string `json:"vendor_id"`
+			Encoder     string `json:"encoder"`
+			Timecode    string `json:"timecode"`
+		} `json:"tags"`
+		SampleFmt      string `json:"sample_fmt,omitempty"`
+		SampleRate     string `json:"sample_rate,omitempty"`
+		Channels       int    `json:"channels,omitempty"`
+		ChannelLayout  string `json:"channel_layout,omitempty"`
+		BitsPerSample  int    `json:"bits_per_sample,omitempty"`
+		InitialPadding int    `json:"initial_padding,omitempty"`
+	} `json:"streams"`
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	Com := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	newBuffer := &bytes.Buffer{}
+	Com.Stdout = newBuffer
+	err := Com.Run()
+	if err != nil {
+		log.Printf("ffprobe command failed: %v\n", err)
+		return "", err
+	}
+	jasonData := VideoStruct{}
+	err = json.Unmarshal(newBuffer.Bytes(), &jasonData)
+	if err != nil {
+		log.Printf("JSON unmarshalling failed: %v\n", err)
+		return "", err
+	}
+	if len(jasonData.Streams) == 0 {
+		return "", fmt.Errorf("no streams found in video")
+	}
+	ratio := math.Round(float64(jasonData.Streams[0].Width)/float64(jasonData.Streams[0].Height)*10) / 10
+	// Use a tolerance to account for slight variations
+	const tolerance = 0.1
+
+	// Check for 16:9 ratio (approximately 1.78)
+	if math.Abs(ratio-16.0/9.0) < tolerance {
+		return "16:9", nil
+	}
+
+	// Check for 9:16 ratio (approximately 0.56)
+	if math.Abs(ratio-9.0/16.0) < tolerance {
+		return "9:16", nil
+	}
+	return "other", nil
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	output := filePath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", output)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("ffmpeg command failed: %v\n", err)
+		return "", err
+	}
+	return output, nil
 }
